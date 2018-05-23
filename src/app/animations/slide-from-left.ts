39865498c023d6064342e719703c73522ddde38e
sarwin-github <dapito.sherwin@yahoo.com>
2018-05-24 01:32:34 +0800

    fixed animation error

--- src/app/animations/slide-from-left.ts
+++ src/app/animations/slide-from-left.ts
@@ -3,15 +3,14 @@ import { trigger, state, style, transition,
 } from '@angular/animations';
 
 export const scrollAnimation = trigger('scrollAnimation', [
-      state('show', style({
-        opacity: 1,
-        transform: "translateX(0)"
-      })),
-      state('hide',   style({
-        opacity: 0,
-        transform: "translateX(-100%)"
-      })),
-      transition('show => hide', animate('700ms ease-out')),
-      transition('hide => show', animate('700ms ease-in'))
-    ])
-]);
+  state('show', style({
+    opacity: 1,
+    transform: "translateX(0)"
+  })),
+  state('hide',   style({
+    opacity: 0,
+    transform: "translateX(-100%)"
+  })),
+  transition('show => hide', animate('700ms ease-out')),
+  transition('hide => show', animate('700ms ease-in'))
+])
