69a34f6e93e0d52c0cc704989436919621c9bc03
Sherwin Dapito <dapito.sherwin@gmail.com>
2019-12-18 16:04:21 +0800

    Added Facebook Pixel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+declare let fbq:Function;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
+	constructor(private router: Router) {
+		// facebook pixel tracker
+		router.events.subscribe((routes: NavigationEnd) => {
+			if(routes instanceof NavigationEnd){
+				fbq('track', 'PageView');
+			}
+		});
+	}
 }
