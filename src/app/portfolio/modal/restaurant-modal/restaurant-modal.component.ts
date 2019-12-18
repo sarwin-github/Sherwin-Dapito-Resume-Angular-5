8438914945080a7110a6d144d916a91561926970
Sherwin Dapito <dapito.sherwin@gmail.com>
2019-12-18 15:59:03 +0800

    updated kamayan sa palaisdaan modal

--- src/app/portfolio/modal/restaurant-modal/restaurant-modal.component.ts
+++ src/app/portfolio/modal/restaurant-modal/restaurant-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'restaurant-modal',
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurant-modal.component.scss']
 })
 export class RestaurantModalComponent implements OnInit {
+	@Input() item: any;
+  	constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  	ngOnInit() {
+  	}
 
 }
